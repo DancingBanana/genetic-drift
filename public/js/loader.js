b5a056199909a245009e524d3ebba7c3400e331a
Dan Applegate <dan@skillshare.com>
2012-11-05 21:22:16 -0500

    Add image require plugin for preloading images

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,8 +1,10 @@
 require.config({
     baseUrl: '/coffee',
     paths: {
+        img: '/img',
         cs: '/js/lib/require/plugins/cs',
         'coffee-script': '/js/lib/require/plugins/coffee-script',
+        image: '/js/lib/require/plugins/image',
         boxbox: '/js/lib/boxbox/boxbox'
     },
     shim: {
